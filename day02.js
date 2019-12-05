3be02098526bdf6dc26743d39d557918e8ef3d8e
Keaton Henderson <keaton@sdsix.com>
2019-12-05 16:59:47 -0700

    completed day02 solution

--- day02.js
+++ day02.js
@@ -2,12 +2,25 @@ import { readIntsFromFile } from './lib/fileIO'
 
 /**
  * Retrieves intcode instruction from puzzle input file.
+ * @return {Array.<int>} the intcode read from the puzze input file.
  */
 let getIntcode = () => {
-    let nums = readIntsFromFile('./puzzle_input/day02_input.txt', ',');
-    nums[1] = 12;
-    nums[2] = 2;
-    return nums;
+    return readIntsFromFile('./puzzle_input/day02_input.txt', ',');
+};
+
+/**
+ * Adds a noun and verb to indices 1 and 2 respectively.
+ * @param {int} noun 
+ * @param {int} verb 
+ * @param {Array.<int>} intcode the intcode to modify. 
+ * @note The given intcode array isn't modified. Instead, a copy is made.
+ * @return {Array.<int>} the modified intcode.
+ */
+let applyNounVerb = (noun, verb, intcode) => {
+    let code = [...intcode];
+    code[1] = noun;
+    code[2] = verb;
+    return code;
 };
 
 /**
@@ -28,7 +41,6 @@ let executeIntcode = (intcode) => {
  * @return {boolean} true if the operation is not a termination opcode.
  */
 let readIntcodeSegment = (intcodeSegment, intcode) => {
-
     let operation = intcodeSegment[0];
 
     if (operation == 99) { return false; }
@@ -52,9 +64,39 @@ let readIntcodeSegment = (intcodeSegment, intcode) => {
     return true;
 };
 
+/**
+ * Finds the combination of noun and verb needed to find the target at index 0 after the intcode is executed.
+ * @param {int} target the desired value at index 0 after the intcode is executed. 
+ * @param {Array.<int>} intcode the intcode to execute.
+ * @return {int} 100 * noun + verb, where the noun and verb will acheive the given target.
+ */
+let findNounVerb = (target, intcode) => {
+    let solutionFound = false;
+    let solution;
+    for (let noun = 0; noun < 99; noun++) {
+        for (let verb = 0; verb < 99; verb++) {
+            let modifiedIntcode = applyNounVerb(noun, verb, intcode);
+            executeIntcode(modifiedIntcode);
+            if (modifiedIntcode[0] == target) {
+                solution = 100 * noun + verb;
+                solutionFound = true;
+                break;
+            }
+        }
+        if (solutionFound) { break; }
+    }
+    return solution;
+}
+
 (() => {
     let intcode = getIntcode();
-    let originalIntcode = [...intcode];
+    let originalIntcode = [...intcode]; // Create a copy of the original intcode.  
+    
+    intcode = applyNounVerb(12, 2, intcode);
     executeIntcode(intcode);
     console.log(`Solution Part 1: ${ intcode[0] }`);
+
+    let solution = findNounVerb(19690720, originalIntcode);
+    console.log(`Solution Part 2: ${ solution }`);
+
 })();
