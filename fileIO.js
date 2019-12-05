d99e7cae9dbda1d41d0e77d8027374d312ceb68c
Keaton Henderson <keaton@sdsix.com>
2019-12-05 14:43:04 -0700

    added optional delimiter to fileio

--- fileIO.js
+++ fileIO.js
@@ -2,13 +2,16 @@
 var fs = require('fs');
 
 /**
- * 
- * @param {String} filepath 
+ * Reads a collection of ints from a given file.
+ * @param {String} filepath the path to the file.
+ * @param {String} [delimiter="\n"] optional delimiter.
+ * @return {Array.<int>} the ints in the file.
  */
-export function readIntsFromFile(filepath) {
+export function readIntsFromFile(filepath, delimiter) {
+    let delimiter = delimiter || "\n"; // Define the delimiter if it's not supplied.
     let fileContents = fs.readFileSync(filepath, 'utf8');
     let numbers = []
-    fileContents.split("\n").forEach(num => {
+    fileContents.split(delimiter).forEach(num => {
         numbers.push(parseInt(num));
     });
     return numbers;
